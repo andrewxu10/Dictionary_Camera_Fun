6875299ef7b651149c3a595a2aa500930cf53782
Andrew Xu <andrewxu_2016@depauw.edu>
2016-02-24 13:13:51 -0500

    update

--- app/src/main/java/cs193a/com/dictionary/DictionaryActivity.java
+++ app/src/main/java/cs193a/com/dictionary/DictionaryActivity.java
@@ -31,23 +31,23 @@ public class DictionaryActivity extends AppCompatActivity implements AdapterView
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dictionary);
+        setContentView(R.layout.activity_dictionary); //open view of activity.dictionary.xml
 
-        ListView definitionList = (ListView) findViewById(R.id.definition_list);
-        definitionList.setOnItemClickListener(this);
-        definitions = new ArrayList<>();
-        adapter = new ArrayAdapter<>(
+        ListView definitionList = (ListView) findViewById(R.id.definition_list); //grab the listview and set it to name 'definitionList'
+        definitionList.setOnItemClickListener(this); //initialize onClickListener here..
+        definitions = new ArrayList<>(); //new arrayList which will house the definitions (which will need to be put into an arrayList)
+        adapter = new ArrayAdapter<>( //adapt between the arrayList java file and arrayList XML in strings.xml. Necessary to keep XML listView dynamic (activity_dictionary.xml)
                 this,
-                R.layout.mydictionarylistlayout,
+                R.layout.mydictionarylistlayout, //individual layout for each line on listView
                 R.id.the_item_text,
                 definitions
         );
 
         //ListView definitionList = (ListView) findViewById(R.id.definition_list);
-        definitionList.setAdapter(adapter);
 
-        readAllDefinitions();
-        pick5randomWords();
+        definitionList.setAdapter(adapter); //set adapter initialized above to the definitionList listView/arrayList
+        readAllDefinitions(); //read in all definitions/words to hashmap
+        pick5randomWords(); //picks 5 random words, scrambles etc
 
         //ArrayList<String> list = new ArrayList<>();
         //dictionary = new HashMap<>();
@@ -56,6 +56,7 @@ public class DictionaryActivity extends AppCompatActivity implements AdapterView
 
 
 
+   //unnecessary method for now
     public void lookup(View view) {
         /*EditText the_word = (EditText) findViewById(R.id.theWord);
         String word = the_word.getText().toString();
@@ -76,45 +77,50 @@ public class DictionaryActivity extends AppCompatActivity implements AdapterView
 
     }
 
-
+    //when the app is initialized, the program needs to read all definitions into a hashmap and split the words from definitions using "/"
     private void readAllDefinitions() {
         dictionary = new HashMap<>();
         Scanner scan = new Scanner(
-                getResources().openRawResource(R.raw.grewords));
+                getResources().openRawResource(R.raw.grewords)); //initialize hashmap + scanner for text file @ grewords.txt in res/raw
         while (scan.hasNextLine()) {
             String line = scan.nextLine();
-            String[] pieces = line.split("/");
+            String[] pieces = line.split("/"); //split each line into word/definition separated by /
+
+
             // pieces = {"abate", "to lessen to subside"}
             //Log.d("helpppppp", "dictionary = " + dictionary); //print errors!
             //Log.d("helpppppp", "pieces = " + pieces); //print errors!
+
+
             if (pieces.length >= 2) {
-                dictionary.put(pieces[0], pieces[1]);
+                dictionary.put(pieces[0], pieces[1]); //if applicable, add them into the hashmap dictionary - word string in 0 and definition string in 1
             }
         }
     }
 
+    //pick 5 random words from arrayList
     public void pick5randomWords() {
         ArrayList<String> chosenWords = new ArrayList<>(dictionary.keySet()); //gets all words in the arrayList
-        Collections.shuffle(chosenWords);
+        Collections.shuffle(chosenWords); //shuffle arrayList of all words to randomize
 
-        word = chosenWords.get(0); //the 'correct' definition
-        TextView the_word = (TextView) findViewById(R.id.theWord);
+        word = chosenWords.get(0); //the 'correct' definition (always initially at the 0 index)
+        TextView the_word = (TextView) findViewById(R.id.theWord); //findthe TextView @ the top where the trivia word is displayed
         the_word.setText(word); //set the word up top
 
-        definitions.clear();
-        for (int i = 0; i < 5; i++) {
+        definitions.clear(); //clear the arrayList for definitions (declared all the way up top)
+        for (int i = 0; i < 5; i++) { //get 5 random words from chosenWords (list of all words, but shuffled)
             String defn = dictionary.get(chosenWords.get(i));
             definitions.add(defn);
         }
 
         Collections.shuffle(definitions); //shuffle again so the first answer isn't always the right one
 
-        //update the listview to store 5 dynamic items
+        //notify android to update the listview in order to store 5 dynamic items
         adapter.notifyDataSetChanged();
 
         }
 
-    private static final int REQ_CODE_TAKE_PICTURE = 30210;
+    //private static final int REQ_CODE_TAKE_PICTURE = 30210; //constant used for photo taking functionality (to map the occurance/data)
 
     /*public void takePicture(View view) {
         Intent picIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
